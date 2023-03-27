c8b5a7f5b756ea339dece0359c19018231b08a96
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-03-27 16:20:53 +0300

    add DI and Twig

--- public_html/index.php
+++ public_html/index.php
@@ -5,11 +5,26 @@ use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+$container = new \DI\Container();
+AppFactory::setContainer($container);
+
+// Инициализация приложения
 $app = AppFactory::create();
 
-$app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
+
+$container = $app->getContainer();
+$container->set('view', function(\Psr\Container\ContainerInterface $container){
+    return new \Slim\Views\Twig('');
+});
+
+// Добавление промежуточного ПО обработки ошибок
+$app->addErrorMiddleware(true, false, false);
+
+// Добавления маршрута с обратным вызовом
+$app->get('/', function (Request $request, Response $response, array $args) {
+    $response->getBody()->write('Hello World');
     return $response;
 });
 
+// Запуск приложения
 $app->run();
